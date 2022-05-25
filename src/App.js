be87dd033a2911a9bd5c93fdd6c21d14e628d4eb
vcoetoG <vcoeto@hotmail.com>
2022-05-25 12:33:47 -0600

    added firebase functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
-import GoogleLogin from 'react-google-login';
-import { signIn } from './util/firebase_config';
+import AuthPage from './pages/Auth';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={signIn}>Sign in with Google</button>
+        <AuthPage/>
       </header>
     </div>
   );
